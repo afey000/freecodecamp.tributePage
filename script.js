2bccfc3af60d59293748c51b2b101f30517f234a
Ahmed Afrah <afey000@gmail.com>
2016-03-06 01:29:09 +0500

    added all project files

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("page").addClass("animated slideInUp")
+  $(".navbar-right").addClass("animated fadeIn ");
+  $(".opt1").addClass("animated bounce");
+  $(".opt2").addClass("animated bounce");
+  $(".opt3").addClass("animated bounce");
+
+
+
+});
