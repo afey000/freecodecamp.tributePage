b33fb806729bfbec24c523244e23a852ed077f95
Ahmed Afrah <afey000@gmail.com>
2016-03-06 13:51:12 +0500

    added a video and fixed navbar problem

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-  $("page").addClass("animated slideInUp")
   $(".navbar-right").addClass("animated fadeIn ");
-  $(".opt1").addClass("animated bounce");
-  $(".opt2").addClass("animated bounce");
-  $(".opt3").addClass("animated bounce");
+  $(".navbar-brand").addClass("animated fadeIn");
+  $(".nav-opt").addClass("animated bounceInDown");
+  $(".page").addClass("animated fadeIn")
+
 
 
 
